86e8a339a2c2bf893ad815a6bdb9c9b7d8ab7d2a
GallaFrancesco <francesco.galla3@gmail.com>
2018-06-26 15:29:36 +0200

    urxvt, mutt

--- mutt/.mutt/pass.py
+++ mutt/.mutt/pass.py
@@ -1,8 +1,8 @@
 import subprocess
 
 def mailpasswd(account):
-    if account == 'unito':
-        path = "/home/fra/.mutt/omappass.gpg"
+    if account == 'fragal':
+        path = "/home/fra/.mutt/passwd.gpg"
         return subprocess.check_output(["gpg", "--quiet", "--batch", "-d", path]).strip()
     else:
         return ''
