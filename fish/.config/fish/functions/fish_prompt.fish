3dfc04c30d3c656331ce9322e684bec359842a77
GallaFrancesco <francesco.galla3@gmail.com>
2019-02-07 12:59:57 +0100

    prompt

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,4 +1,6 @@
 function fish_prompt --description 'Write out the prompt'
+	set -l st $status
+
 	if not set -q __fish_prompt_normal
         set -g __fish_prompt_normal (set_color normal)
     end
@@ -7,7 +9,12 @@ function fish_prompt --description 'Write out the prompt'
         set -g __fish_prompt_cwd (set_color $fish_color_cwd)
     end
 
-    echo -n -s "$USER" @ (prompt_hostname) ' ' "$__fish_prompt_cwd" (prompt_pwd) \
-	(__fish_vcs_prompt)  ' [' (python /home/fra/.config/fish/checkunreadmail.py) '] ' \
-	"$__fish_prompt_normal" '> '
+	if test $st -eq 0
+		set -e st
+	else
+		set st " [$st]"
+	end
+
+    echo -n -s '[' (python /home/fra/.config/fish/checkunreadmail.py) '] '"$USER" "$__fish_prompt_cwd"  \
+	 (__fish_vcs_prompt) ' ' (prompt_pwd) (echo $st) "$__fish_prompt_normal" '> '
 end
