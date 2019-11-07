21a1b61d5bc8f03ca9e3808b8b3817d87f2e7230
GallaFrancesco <francesco.galla3@gmail.com>
2019-11-07 12:08:14 +0100

    thinkpad scripts in branch

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -9,7 +9,7 @@ function fish_prompt --description 'Write out the prompt'
         set -g __fish_prompt_cwd (set_color $fish_color_cwd)
     end
 
-    echo -n -s "$USER" @ (prompt_hostname) ' ' "$__fish_prompt_cwd" (prompt_pwd) \
+    echo -n -s "$USER" ' ' "$__fish_prompt_cwd" (prompt_pwd) \
 	(__fish_vcs_prompt)    \
 	"$__fish_prompt_normal" '> '
 end
