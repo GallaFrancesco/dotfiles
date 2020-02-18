3fcf12ef6512a4634f8d7a80ac1a262b444e6813
GallaFrancesco <francesco.galla3@gmail.com>
2020-02-18 10:50:11 +0100

    org mode latex

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -123,6 +123,17 @@
 (global-set-key (kbd "C-c") 'org-capture)
 (global-set-key (kbd "C-b") 'org-switchb)
 
+(with-eval-after-load 'ox-latex
+    (add-to-list 'org-latex-classes
+                '("report-noparts"
+                "\\documentclass{report}"
+                ("\\chapter{%s}" . "\\chapter*{%s}")
+                ("\\section{%s}" . "\\section*{%s}")
+                ("\\subsection{%s}" . "\\subsection*{%s}")
+                ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+                ("\\paragraph{%s}" . "\\paragraph*{%s}")
+                ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))))
+
 ;; supercollider (scel)
 (add-to-list 'load-path "~/_progs/scel/el")
 (require 'sclang)
