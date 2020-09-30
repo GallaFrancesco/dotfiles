a7c48c619e0239b15cf59117480400f29cc87556
GallaFrancesco <francesco.galla3@gmail.com>
2020-09-30 16:14:05 +0200

    others

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,3 +2,4 @@ set PATH $PATH /usr/local/bin /home/fra/.local/bin/ /usr/local/GreatSPN/bin:/usr
 set -x EDITOR vim
 set -x PAGER most
 export _JAVA_AWT_WM_NONREPARENTING=1
+export GUILE_LOAD_COMPILED_PATH="/usr/local/lib/guile/2.2/ccache:/usr/local/lib/guile/2.2/site-ccache"
---
+++ fish/.config/fish/functions/schelp.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function schelp --description 'alias schelp=waterfox "doc.sccode.org"'
+	waterfox "doc.sccode.org" $argv;
+end
